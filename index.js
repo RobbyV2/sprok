306c5d20c14db3af1568a8b1a535429459442185
Hitu-C <robby@robby.blue>
2023-12-03 16:01:44 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const session = require('express-session');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const bodyParser = require('body-parser');
+const cheerio = require('cheerio');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(session({
+  secret: '1324',
+  resave: false,
+  saveUninitialized: true,
+}));
+
+app.get('/', (req, res) => {
+    res.redirect('/login.html');
+  });
+
+app.post('/login.html', (req, res) => {
+  if (req.body.username === '1324' && req.body.password === '1324') {
+    req.session.loggedIn = true;
+    res.redirect('/spork/');
+  } else {
+    res.redirect('/login.html');
+  }
+});
+
+app.use('/spork/', (req, res, next) => {
+console.log('Redirected URL:', 'https://robby.blue' + req.url);
+  if (req.session.loggedIn) {
+    next();
+  } else {
+    res.redirect('/login.html');
+  }
+});
+
+app.use('*', (req, res, next) => {
+  var _write = res.write;
+  var _end = res.end;
+
+  var isHtml = false;
+  var chunks = [];
+
+  res.write = function (chunk) {
+    if (res.get('Content-Type') && res.get('Content-Type').includes('text/html')) {
+      isHtml = true;
+      chunks.push(new Buffer(chunk));
+    } else {
+      _write.call(res, chunk);
+    }
+  };
+
+  res.end = function (chunk) {
+    if (isHtml) {
+      if (chunk) chunks.push(new Buffer(chunk));
+
+      var body = Buffer.concat(chunks).toString();
+      const $ = cheerio.load(body);
+
+      $('title').text('SPORKforYou');
+
+      $('link[rel="icon"]').attr('href', 'https://app.spork.school/favicon.ico');
+
+      var html = $.html();
+
+      html += `
+        <script>
+    document.addEventListener('keydown', function(e) {
+        if (e.ctrlKey) {
+            e.preventDefault();
+            window.location.href = window.location.pathname === '/login.html' ? '/spork/' : '/login.html';
+        }
+    });
+        </script>
+      `;
+
+      res.setHeader('Content-Length', Buffer.byteLength(html));
+
+      _end.call(res, Buffer.from(html));
+    } else {
+      if (chunk) _write.call(res, chunk);
+      _end.call(res);
+    }
+  };
+
+  next();
+});
+
+app.use('/spork/', createProxyMiddleware({ 
+  target: 'https://robby.blue', 
+  changeOrigin: true,
+  pathRewrite: {
+    '^/spork/': '/', // remove base path
+  },
+}));
+
+app.listen(3009);
+console.log("Sprok started -> http://localhost:3009");
---
+++ readme.md
@@ -0,0 +1,6 @@
+## Sprok
+This repo contains the source for a spork clone. This website is meant to act as an intermediary layer between a website and a fake [spork](https://app.spork.school/) interface.
+
+## TODO
+- For copying spork, at least for the login and main menu just paste an image.
+- Add subdomain support for any original domain (e.g. ```google.com -> test.google.com, localhost -> test.localhost```)
